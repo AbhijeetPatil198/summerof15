27ee41012acc5314c232a637b92478a29a940089
a <a@ubuntu.ubuntu-domain>
2015-06-14 21:25:23 +0530

    added jquery source and buggy code

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function goNLP(info,tab) {
 
     console.log("Word " + info.selectionText + " was clicked.");
+    
     chrome.tabs.create({ 
+    	url: "http://www.google.com/search?q=" + info.selectionText,      
+    });
 
-        url: "http://www.google.com/search?q=" + info.selectionText,
-    })
 
+    var txt = info.selectionText;
+
+
+    $.post("http://text-processing.com/api/sentiment/", {text: "great"}, function(data,status){
+    	alert('fuck1');
+    	alert(status);
+    	
+    });
+
+    //alert('fuck');
 
 }
 
