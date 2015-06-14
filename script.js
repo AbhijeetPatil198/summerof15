a242736289a253fa5d0862098c7bb76d12ca756c
a <a@ubuntu.ubuntu-domain>
2015-06-14 19:40:21 +0530

    new tab opens and searches on googl

---
+++ script.js
@@ -0,0 +1,18 @@
+function goNLP(info,tab) {
+
+    console.log("Word " + info.selectionText + " was clicked.");
+    chrome.tabs.create({ 
+
+        url: "http://www.google.com/search?q=" + info.selectionText,
+    })
+
+
+}
+
+chrome.contextMenus.create({
+
+    title: "Process: %s", 
+    contexts:["selection"], 
+    onclick: goNLP,
+
+});
