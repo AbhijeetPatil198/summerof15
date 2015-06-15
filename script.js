db160a21ab210f22f7559e80434c96b1c8e7d42b
a <a@ubuntu.ubuntu-domain>
2015-06-15 18:45:57 +0530

    request synced

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 function goNLP(info,tab) {
-
+	//write in console
     console.log("Word " + info.selectionText + " was clicked.");
-    
+    //open a new tab
     chrome.tabs.create({ 
     	url: 'index.html'     
     });
+    //some variables
+    var query = info.selectionText;
+    var res_data = '';
+	//interaction with server
+	$.ajax({
 
+  		type: "POST",
 
-    var txt = info.selectionText;
+  		url:"http://text-processing.com/api/sentiment/",
 
-	
-	$.post("http://text-processing.com/api/sentiment/", {'text': "great"}, 
-	function(data, status){ alert(JSON.stringify(data));}
-	,"json" );
-  
+ 		data: { text : query },
+
+ 		success: function(data, status){ res_data=JSON.stringify(data); },
+
+  		dataType: 'json',
+
+  		async:false
+
+	});
+	//print output in pop up
+	alert(res_data);
 }
 
 
@@ -21,6 +33,6 @@ chrome.contextMenus.create({
 
     title: "Process: %s", 
     contexts:["selection"], 
-    onclick: goNLP,
+    onclick: goNLP
 
 });
