a237f3d9106f03a4c10ee7d34789637356cf1205
a <a@ubuntu.ubuntu-domain>
2015-06-15 14:45:59 +0530

    working in popup

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ function goNLP(info,tab) {
     console.log("Word " + info.selectionText + " was clicked.");
     
     chrome.tabs.create({ 
-    	url: "http://www.google.com/search?q=" + info.selectionText,      
+    	url: 'index.html'     
     });
 
 
     var txt = info.selectionText;
 
-
-    $.post("http://text-processing.com/api/sentiment/", {text: "great"}).done( function(msg) { } )
-    .fail( function(xhr, textStatus, errorThrown) {
-        alert(xhr.responseText);
-    });
-    
+	
+	$.post("http://text-processing.com/api/sentiment/", {'text': "great"}, 
+	function(data, status){ alert(JSON.stringify(data));}
+	,"json" );
+  
 }
+
 
 chrome.contextMenus.create({
 
